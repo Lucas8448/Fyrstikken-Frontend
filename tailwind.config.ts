117a910838f845aee4cf0d8bb3467232f6f05ff5
Lucas William Bateson <lucas.bateson@gmail.com>
2024-06-19 12:55:57 +0200

    Add winner function

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config = {
     },
     extend: {
       colors: {
-
+        gold: '#FFD700',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
